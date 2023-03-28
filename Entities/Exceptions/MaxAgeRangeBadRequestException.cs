82fc5bd31018a5ca8ebf40e49ca62794fbe6eca7
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-03-28 02:18:43 +0100

    add: filtering feature was added

---
+++ Entities/Exceptions/MaxAgeRangeBadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions;
+
+public sealed class MaxAgeRangeBadRequestException : BadRequestException
+{
+    public MaxAgeRangeBadRequestException() : base("MaxAge can not be less than MinAge")
+    {
+        
+    }
+}
