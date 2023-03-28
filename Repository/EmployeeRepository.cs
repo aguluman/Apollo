9c4cf5d851079e93222552acd537d58403ba21f6
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-03-28 09:51:52 +0100

    add: searching feature for employees in RepositoryEmployeeExtensions.cs

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Contracts;
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
 using Shared.RequestFeatures;
 
 namespace Repository;
@@ -16,18 +17,17 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
         Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
     {
         var employees = await FindByCondition(
-                e => e.CompanyId.Equals(companyId)
-                     && e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge,
-                trackChanges)
+                e => e.CompanyId.Equals(companyId), trackChanges)
+            .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+            .Search(employeeParameters.SearchTerm)
             .OrderBy(e => e.Name)
             .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
             .Take(employeeParameters.PageSize)
             .ToListAsync();
 
-        var count = await FindByCondition(
-            e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
-        return new PagedList<Employee>
-            (employees, count, employeeParameters.PageNumber, employeeParameters.PageSize);
+        
+        return PagedList<Employee>
+            .ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
     }
 
     public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
